01f30ebe2dbf76ad90b6a14b378400618c5c439c
leospolowicz <79314147+leospolowicz@users.noreply.github.com>
2021-03-12 12:09:00 +0100

    Add files via upload

---
+++ leohellomarch.py
@@ -0,0 +1,20 @@
+
+print("hello world 1")
+
+przywitanie = 'hello world 2'
+
+print(przywitanie)
+
+h1 = 'hello'
+h2 = 'world'
+i1 = 3
+print(h1, h2, i1)
+
+j1 = 4.
+
+print(h1, h2, j1)
+
+print("printujemy typy znmiennych")
+print("typ zmiennej s1", type(h1))
+print("typ zmiennej i1", type(i1))
+print("typ zmiennej f1", type(j1))
